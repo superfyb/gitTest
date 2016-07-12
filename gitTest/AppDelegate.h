ab46dc264f64a4b1b11bc3dda2db2cbc53ebbae8
fyb <13777859537@163.com>
2016-07-12 14:47:31 +0800

    第一次提交

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by web2 on 16/7/12.
+//  Copyright © 2016年 fyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by web2 on 16/7/12.
+//  Copyright © 2016年 fyb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"git_test_1");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
